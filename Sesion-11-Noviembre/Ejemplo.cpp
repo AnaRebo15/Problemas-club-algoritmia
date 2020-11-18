8290cd92fd4a69804d4455c36cac67dcb3f72c53
AnaRebo15 <72528458+AnaRebo15@users.noreply.github.com>
2020-11-18 17:06:49 -0600

    Delete Ejemplo.cpp

--- Sesion-11-Noviembre/Ejemplo.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-	auto p = make_pair(1, 3.14);
-	cout<<"("<<get<0>(p)<<", "<<get<1>(p)<<")\n";
-	cout<<"("<<get<int>(p)<<", "<<get<double>(p)<<")\n";
-	cout<<"("<<get<double>(p)<<", "<<get<int>(p)<<")\n";
-}
