a6ed8033d9ea8422725638f789d713de5f2d9442
AnaRebo15 <72528458+AnaRebo15@users.noreply.github.com>
2020-11-18 18:07:54 -0600

    Create Ejemplo2.cpp

---
+++ Sesion-18-Noviembre/tuple/Ejemplo2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	tuple<int, string, string> alumno;
+	alumno = make_tuple(1, "Julio", "Perez");
+
+	//Imprimir el tamaño de la tupla.
+	cout<<tuple_size<decltype(alumno)>::value<<endl;
+	return 0;
+}
